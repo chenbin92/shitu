9b7320815abf63bf99edf2b8e390847e0d4dcb77
Bruce Chen <bruce@beansmile.com>
2015-07-19 18:38:50 +0800

    Basic layout

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,14 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'shitu' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('shitu', ['ionic'])
+var app = angular.module('shitu', [
+  'ionic',
+  'shitu.routes'
+])
 
-.run(function($ionicPlatform) {
+var shitu = shitu || {};
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
