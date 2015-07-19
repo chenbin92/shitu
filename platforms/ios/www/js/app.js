10a8ef55d40fb078c5aae374a6f2f2ccfbdbba6d
Bruce Chen <bruce@beansmile.com>
2015-07-19 20:50:33 +0800

    Add the ios platform files

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,11 +1,16 @@
 // Ionic Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'shitu' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('shitu', [
+  'ionic',
+  'shitu.routes'
+])
 
-.run(function($ionicPlatform) {
+var shitu = shitu || {};
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
